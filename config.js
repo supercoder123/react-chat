b5e77a509caa1b9eee99766ec5bb4dd4ffcb0520
Ashley Fernandes <ashleefernandes@gmail.com>
2018-08-13 19:24:13 +0530

    setup keys

---
+++ config.js
@@ -0,0 +1,5 @@
+const tokenUrl =
+  "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/abb5faaf-7944-448c-9609-080278410299/token";
+const instanceLocator = "v1:us1:abb5faaf-7944-448c-9609-080278410299";
+
+export { tokenUrl, instanceLocator };
